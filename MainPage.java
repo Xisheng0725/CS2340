1acd185e4db1f9f266e0c9906c90f53b1559df09
Xisheng0725 <98372495+Xisheng0725@users.noreply.github.com>
2022-06-01 11:03:14 -0400

    Update MainPage.java

--- MainPage.java
+++ MainPage.java
@@ -2,6 +2,17 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.geometry.Pos;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.FontPosture;
+
+
 /**
 *Represents MainPage named MainPage.
 *@author Xisheng Zhang, Qihui Wang, Porter Zach, Xueqing Li, and Shane Sinnerine.
@@ -23,8 +34,20 @@ public class MainPage extends Application {
     public void start(Stage stage) {
         stage.setTitle("Game Suit"); // When can change when we find a better name.
         StackPane sp = new StackPane();
+        BorderPane bp = new BorderPane();
         Scene scene = new Scene(sp, 1300, 900);
         stage.setScene(scene);
+
+        Text tx1 = new Text("Game Name"); // when can decide later
+        tx1.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 50));
+
+        Button bt = new Button("Start game");
+        VBox vbox = new VBox(400);
+        vbox.getChildren().addAll(tx1, bt);
+        vbox.setAlignment(Pos.CENTER);
+        bp.setCenter(vbox);
+        sp.setAlignment(bp, Pos.CENTER);
+        sp.getChildren().addAll(bp);
 
         stage.show();
     }
