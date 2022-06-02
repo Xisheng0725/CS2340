e303c4b18296c78ef14d69f5bf91dc103ac66f94
Xisheng0725 <98372495+Xisheng0725@users.noreply.github.com>
2022-06-02 18:01:57 -0400

    Update MainPage.java

--- MainPage.java
+++ MainPage.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
@@ -13,17 +14,18 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.FontPosture;
 import javafx.event.ActionEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 
 
 /**
 *Represents MainPage named MainPage.
-* Note: Please add commons or JavaDocs description when you add something
 *@author Xisheng Zhang, Qihui Wang, Porter Zach, Xueqing Li, and Shane Sinnerine.
 *@version 1.0.
 */
 public class MainPage extends Application {
     /**
     * Creat the main method.
+    * Please add commons or JavaDocs description when you add something
     * @param args take in
     */
     public static void main(String[] args) {
@@ -35,9 +37,9 @@ public class MainPage extends Application {
     */
     public void start(Stage stage) {
         stage.setTitle("Game Suit"); // When can change when we find a better name.
-        StackPane sp = new StackPane();
-        BorderPane bp = new BorderPane();
-        Scene scene = new Scene(sp, 1300, 900);
+        StackPane firstMain = new StackPane();
+        BorderPane firstBP = new BorderPane();
+        Scene scene = new Scene(firstMain, 1300, 900);
         stage.setScene(scene);
 
         Text tx1 = new Text("Game Name"); // when can decide later
@@ -45,11 +47,31 @@ public class MainPage extends Application {
 
         Button bt = new Button("Start game");
         VBox vbox = new VBox(400);
-        vbox.getChildren().addAll(tx1, bt);  // I made a VBox to hold the game name and button for start the game.
+        vbox.getChildren().addAll(tx1, bt);
         vbox.setAlignment(Pos.CENTER);
-        bp.setCenter(vbox);
-        sp.setAlignment(bp, Pos.CENTER);  // set center
-        sp.getChildren().addAll(bp); // the stackPane hod the Boderpane.
+        firstBP.setCenter(vbox);
+        firstMain.setAlignment(firstBP, Pos.CENTER);
+        firstMain.getChildren().addAll(firstBP);
+
+
+        // second main page which show 3 games.
+        StackPane secondMain = new StackPane();
+        BorderPane secondBP = new BorderPane();
+        Scene scene2 = new Scene(secondMain, 1300, 900);
+        HBox hbox = new HBox(100);
+
+        Rectangle game1 = new Rectangle(250, 250);
+        Rectangle game2 = new Rectangle(250, 250);
+        Rectangle game3 = new Rectangle(250, 250);
+        hbox.getChildren().addAll(game1, game2, game3);
+        hbox.setAlignment(Pos.CENTER);
+        secondBP.setCenter(hbox);
+        secondMain.setAlignment(secondBP, Pos.CENTER);
+        secondMain.getChildren().addAll(secondBP);
+
+        bt.setOnAction(e -> stage.setScene(scene2));
+
+
 
         stage.show();
     }
