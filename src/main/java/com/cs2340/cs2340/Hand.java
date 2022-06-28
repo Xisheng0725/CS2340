0b71bc3e88cad859eb5199b7919c1c42fc80ce44
Shane Sinnerine <shanesinnerine@gmail.com>
2022-06-27 21:29:55 -0400

    added hand functionality

--- src/main/java/com/cs2340/cs2340/Hand.java
+++ src/main/java/com/cs2340/cs2340/Hand.java
@@ -1,22 +1,35 @@
 package com.cs2340.cs2340;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hand {
+    ArrayList<Card> hand;
     public Hand() {
-
+        hand = new ArrayList<>();
     }
 
     public void addCard(Card card) {
-
+        hand.add(card);
     }
 
     public int getValue() {
-        // will need to count aces as either 11 or 1 depending on remaining value of hand
-        throw new UnsupportedOperationException("not implemented yet.");
+        int aceCount = 0;
+        int total = 0;
+        for(Card card : hand) {
+            if(card.getBlackjackValue() == 11) {
+                aceCount++;
+            }
+            total += card.getBlackjackValue();
+        }
+        while(total > 21 && aceCount != 0) {
+            total -= 10;
+            aceCount--;
+        }
+        return total;
     }
 
     public List<Card> getHand() {
-        throw new UnsupportedOperationException("not implemented yet.");
+        return this.hand;
     }
 }
